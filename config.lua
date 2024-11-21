49f36e48cd6316b4e85f5d6673537fd6c07a9da5
Stoic <112611821+TheStoicBear@users.noreply.github.com>
2024-11-20 21:59:16 -0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,27 +1,2 @@
-fx_version 'cerulean'
-game 'gta5'
-
-author 'Stoic'
-description 'Character clothing sync using five-appearance and ND Framework'
-version '1.0.0'
-
--- Configuration files
-shared_scripts {
-    'config.lua' -- Config file to manage debug settings
-}
-
--- Client-side scripts
-client_scripts {
-    'client.lua'
-}
-
--- Server-side scripts
-server_scripts {
-    'server.lua'
-}
-
--- Dependencies
-dependencies {
-    'five-appearance', -- Ensures the five-appearance resource is running
-    'ND_Characters'     -- Ensures ND Framework is available
-}
+Config = {}
+Config.debug = true -- Set to true to enable debugging
